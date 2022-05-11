aa3fdd80db1a2c8070dbec90edcdc4ef9174bf36
Diego Zacarias <diego.a.z.h@gmail.com>
2022-05-11 14:45:41 -0600

    buildpacks updates

--- lib/dependencies.sh
+++ lib/dependencies.sh
@@ -235,7 +235,7 @@ npm_node_modules() {
     if [[ "$(should_use_npm_ci "$build_dir")" == "true" ]] && [[ "$USE_NPM_INSTALL" != "true" ]]; then
       meta_set "use-npm-ci" "true"
       echo "Installing node modules $build_dir"
-      echo "current path $(cd pwd)"
+      echo "current path $(pwd)"
       monitor "npm-install" npm ci --production="$production" --unsafe-perm --userconfig "$build_dir/.npmrc" 2>&1
     else
       meta_set "use-npm-ci" "false"
